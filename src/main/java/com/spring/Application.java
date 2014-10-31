25f79d16fd12cc0c9bf1791df430477c86f165ac
Kleber Mota <klebermo@gmail.com>
2014-10-31 09:28:32 -0300

    adding FormPublic / FormSettings and updating main class

--- src/main/java/com/spring/Application.java
+++ src/main/java/com/spring/Application.java
@@ -1,13 +1,23 @@
 package com.spring;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.config.annotation.form.Form;
+import com.spring.config.annotation.form.FormPublic;
+import com.spring.config.annotation.form.FormSettings;
 
 @Controller
 @EnableJpaRepositories
@@ -26,12 +36,14 @@ public class Application {
 	
 	@RequestMapping(value = "/admin")
 	@PreAuthorize("hasPermission(#user, 'admin')")
-	public String admin(Model model) {
+	public String admin(Model model) throws Exception {
+		model.addAttribute("menu", lista_private());
 		return "private/admin";
 	}
 	
 	@RequestMapping(value = "/")
-	public String index(Model model) {
+	public String index(Model model) throws Exception {
+		model.addAttribute("menu", lista_public());
 		return "public/index";
 	}
 	
@@ -41,8 +53,45 @@ public class Application {
 	}
 	
 	@RequestMapping(value = "/settings")
-	public String settings(Model model) {
+	public String settings(Model model) throws Exception {
+		model.addAttribute("settings", lista_settings());
 		return "private/settings";
+	}
+	
+	private List<Class<?>> lista_private() throws Exception {
+		List<Class<?>> lista = new ArrayList<Class<?>>();
+		
+		ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
+		scanner.addIncludeFilter(new AnnotationTypeFilter(Form.class));
+		for (BeanDefinition bd : scanner.findCandidateComponents("com.spring.model")) {
+			lista.add(Class.forName(bd.getBeanClassName()));
+		}
+		
+		return lista;
+	}
+	
+	private List<Class<?>> lista_public() throws Exception {
+		List<Class<?>> lista = new ArrayList<Class<?>>();
+		
+		ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
+		scanner.addIncludeFilter(new AnnotationTypeFilter(FormPublic.class));
+		for (BeanDefinition bd : scanner.findCandidateComponents("com.spring.model")) {
+			lista.add(Class.forName(bd.getBeanClassName()));
+		}
+		
+		return lista;
+	}
+	
+	private List<Class<?>> lista_settings() throws Exception {
+		List<Class<?>> lista = new ArrayList<Class<?>>();
+		
+		ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
+		scanner.addIncludeFilter(new AnnotationTypeFilter(FormSettings.class));
+		for (BeanDefinition bd : scanner.findCandidateComponents("com.spring.model")) {
+			lista.add(Class.forName(bd.getBeanClassName()));
+		}
+		
+		return lista;
 	}
 	
 }
