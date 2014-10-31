dc2ea7e5bd049ff7a634b11a6825eec9dbe25203
Kleber Mota <klebermo@gmail.com>
2014-10-31 20:49:33 -0300

    updating basicService

--- src/main/java/com/spring/config/generic/service/basicService.java
+++ src/main/java/com/spring/config/generic/service/basicService.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 
@@ -25,22 +27,26 @@ public class basicService<E> {
 		this.clazz = clazz;
 	}
 	
+	@Transactional
 	@PreAuthorize("hasPermission(#user, 'cadastra_'+#this.this.name)")
 	public void insert(E object) {
 		dao.persist(object);
 	}
 	
+	@Transactional
 	@SuppressWarnings("unchecked")
 	@PreAuthorize("hasPermission(#user, 'altera_'+#this.this.name)")
 	public E update(E object) {
 		return (E) dao.merge(object);
 	}
 	
+	@Transactional
 	@PreAuthorize("hasPermission(#user, 'remove_'+#this.this.name)")
 	public void remove(E object) {
 		dao.delete(object);
 	}
 	
+	@Transactional
 	@SuppressWarnings("unchecked")
 	public E newObject() throws Exception {
 		return (E) clazz.newInstance();
@@ -51,6 +57,7 @@ public class basicService<E> {
 		return (E) dao.findById(id);
 	}
 	
+	@Transactional
 	public List<?> lista() {
 		return dao.findAll();
 	}
