ab90929ce494ccab5ee077a8dd07767f83332dc3
Kleber Mota <kelbermo@gmail.com>
2014-11-03 18:35:13 -0300

    updating settingdService

--- src/main/java/com/spring/config/generic/service/settingsService.java
+++ src/main/java/com/spring/config/generic/service/settingsService.java
@@ -27,26 +27,35 @@ public class settingsService<E extends Properties> {
 	}
 	
 	public void save_properties(E props) throws Exception {
-		FileInputStream inStream = new FileInputStream( getFilename() );
 		Properties props_current = new Properties();
+		
+		System.out.println("inStream");
+		FileInputStream inStream = new FileInputStream( getFilename() );
 		props.load(inStream);
+		inStream.close();
+		
 		for(Object key:props.entrySet()) {
+			System.out.println(key.toString() + " = " + props.get(key).toString());
 			props_current.setProperty(key.toString(), props.get(key).toString());
 		}
-		inStream.close();
+		
+		System.out.println("outStream");
 		FileOutputStream outStream = new FileOutputStream( getFilename() );
 		props_current.store(outStream, "settings");
 		outStream.close();
 	}
 	
 	public void delete_properties(E props) throws Exception {
-		FileInputStream inStream = new FileInputStream( getFilename() );
 		Properties props_current = new Properties();
+		
+		FileInputStream inStream = new FileInputStream( getFilename() );
 		props_current.load(inStream);
+		inStream.close();
+		
 		for(Object key:props.entrySet()) {
 			props_current.remove(key);
 		}
-		inStream.close();
+		
 		FileOutputStream outStream = new FileOutputStream( getFilename() );
 		props_current.store(outStream, "settings");
 		outStream.close();
