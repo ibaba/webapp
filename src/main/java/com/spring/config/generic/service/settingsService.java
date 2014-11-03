16984029c841cc0854a664ba6b6c79a0c610d641
Kleber Mota <kelbermo@gmail.com>
2014-11-03 17:32:54 -0300

    updating settingsService

--- src/main/java/com/spring/config/generic/service/settingsService.java
+++ src/main/java/com/spring/config/generic/service/settingsService.java
@@ -3,6 +3,10 @@ package com.spring.config.generic.service;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class settingsService<E extends Properties> {
@@ -14,42 +18,59 @@ public class settingsService<E extends Properties> {
 	}
 	
 	public void create_properties(E props) throws Exception {
-		FileInputStream fos = new FileInputStream( getFilename() );
-		Properties props_current = new Properties();
-		props_current.load(fos);
-		for(Object key:props.keySet()) {
-			props_current.setProperty(key.toString(), props.get(key).toString());
+		FileOutputStream outStream = new FileOutputStream( getFilename() );
+		for(int i=0; i<fields().size(); i++) {
+			props.setProperty(fields().get(i), values().get(i));
 		}
-		fos.close();
-		FileOutputStream out = new FileOutputStream( getFilename() );
-		props_current.store(out, "settings");
-		out.close();
+		props.store(outStream, "settings");
+		outStream.close();
 	}
 	
 	public void save_properties(E props) throws Exception {
-		FileInputStream fos = new FileInputStream( getFilename() );
+		FileInputStream inStream = new FileInputStream( getFilename() );
 		Properties props_current = new Properties();
-		props_current.load(fos);
-		for(Object key:props.keySet()) {
+		props.load(inStream);
+		for(Object key:props.entrySet()) {
 			props_current.setProperty(key.toString(), props.get(key).toString());
 		}
-		fos.close();
-		FileOutputStream out = new FileOutputStream( getFilename() );
-		props_current.store(out, "settings");
-		out.close();
+		inStream.close();
+		FileOutputStream outStream = new FileOutputStream( getFilename() );
+		props_current.store(outStream, "settings");
+		outStream.close();
 	}
 	
 	public void delete_properties(E props) throws Exception {
-		FileInputStream fos = new FileInputStream( getFilename() );
+		FileInputStream inStream = new FileInputStream( getFilename() );
 		Properties props_current = new Properties();
-		props_current.load(fos);
-		for(Object key:props.keySet()) {
+		props_current.load(inStream);
+		for(Object key:props.entrySet()) {
 			props_current.remove(key);
 		}
-		fos.close();
-		FileOutputStream out = new FileOutputStream( getFilename() );
-		props_current.store(out, "settings");
-		out.close();
+		inStream.close();
+		FileOutputStream outStream = new FileOutputStream( getFilename() );
+		props_current.store(outStream, "settings");
+		outStream.close();
+	}
+	
+	private List<String> fields() throws Exception {
+		List<String> lista = new ArrayList<String>();
+		
+		Field fields[] = clazz.newInstance().getClass().getDeclaredFields();
+		for(int i=0; i<fields.length; i++)
+			lista.add(fields[i].getName());
+		
+		return lista;
+	}
+	
+	private List<String> values() throws Exception {
+		List<String> lista = new ArrayList<String>();
+		
+		Method methods[] = clazz.newInstance().getClass().getDeclaredMethods();
+		for(int i=0; i<methods.length; i++)
+			if(methods[i].getName().substring(0, 3).equals("get"))
+				lista.add(methods[i].invoke(clazz.newInstance()).toString());
+		
+		return lista;
 	}
 	
 	public String getFilename() {
