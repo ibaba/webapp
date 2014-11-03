48c8c46313570fafb3470a0858673ace8642dec3
Kleber Mota <klebermo@gmail.com>
2014-11-03 09:27:20 -0300

    updating geralService

--- src/main/java/com/spring/model/settings/geral/GeralService.java
+++ src/main/java/com/spring/model/settings/geral/GeralService.java
@@ -1,7 +1,10 @@
 package com.spring.model.settings.geral;
 
+import org.springframework.stereotype.Service;
+
 import com.spring.config.generic.service.settingsService;
 
+@Service
 public class GeralService extends settingsService<Geral> {
 
 	public GeralService() {
