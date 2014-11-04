35bda00fa13d2e9a707cf1c43f7f6b5bc1fa594c
Kleber Mota <klebermo@gmail.com>
2014-11-04 11:02:54 -0300

    updating PropertySourcesPlaceholderConfigurer bean

--- src/main/java/com/spring/config/app/WebAppConfig.java
+++ src/main/java/com/spring/config/app/WebAppConfig.java
@@ -1,14 +1,12 @@
 package com.spring.config.app;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.util.Properties;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -24,21 +22,9 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
     }
 	
     @Bean
-    public PropertySourcesPlaceholderConfigurer property() throws Exception {
+    static PropertySourcesPlaceholderConfigurer property() throws Exception {
     	PropertySourcesPlaceholderConfigurer propertyConfigurer = new PropertySourcesPlaceholderConfigurer();
-    	
-    	Properties props = new Properties();
-    	File file = new File(System.getProperty("user.home")+File.separator+".webapp"+File.separator+"webapp.preferences");
-    	if(file.exists()) {
-        	FileInputStream fos = new FileInputStream( file.getAbsolutePath() );
-        	props.load(fos);
-        	propertyConfigurer.setProperties(props);
-    	} else {
-        	FileOutputStream fos = new FileOutputStream( file.getAbsoluteFile() );
-        	props.store(fos, "settings");
-        	propertyConfigurer.setProperties(props);
-    	}
-    	
+    	propertyConfigurer.setLocation(new FileSystemResource(System.getProperty("user.home")+File.separator+".webapp"+File.separator+"webapp.preferences"));
     	return propertyConfigurer;
     }
 	
