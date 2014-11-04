15f7a650251d00dc04828275265fb6046be7fbe8
Kleber Mota <klebermo@gmail.com>
2014-11-03 22:30:51 -0300

    updating settingsService

--- src/main/java/com/spring/config/generic/service/settingsService.java
+++ src/main/java/com/spring/config/generic/service/settingsService.java
@@ -6,6 +6,8 @@ import java.io.FileOutputStream;
 import java.lang.reflect.Field;
 import java.util.Properties;
 
+import com.spring.config.annotation.settings.Property;
+
 public class settingsService<E> {
 	
 	protected Class<?> clazz;
@@ -18,24 +20,31 @@ public class settingsService<E> {
 		Properties nova = new Properties();
 		FileOutputStream outStream = new FileOutputStream( getFilename() );
 		
-		//
+		Field fields[] = props.getClass().getDeclaredFields();
+		for(int i=0; i<fields.length; i++) {
+			String key = fields[i].getAnnotation(Property.class).key();
+			String value = props.getClass().getMethod("get"+caps(fields[i].getName())).invoke(props).toString();
+			nova.setProperty(key, value);
+		}
 		
 		nova.store(outStream, "settings");
 		outStream.close();
 	}
 	
 	public void save_properties(E props) throws Exception {
-		System.out.println("props = "+props);
 		Properties current = new Properties();
 		
-		System.out.println("inStream");
 		FileInputStream inStream = new FileInputStream( getFilename() );
 		current.load(inStream);
 		inStream.close();
 		
-		//
+		Field fields[] = props.getClass().getDeclaredFields();
+		for(int i=0; i<fields.length; i++) {
+			String key = fields[i].getAnnotation(Property.class).key();
+			String value = props.getClass().getMethod("get"+caps(fields[i].getName())).invoke(props).toString();
+			current.setProperty(key, value);
+		}
 		
-		System.out.println("outStream");
 		FileOutputStream outStream = new FileOutputStream( getFilename() );
 		current.store(outStream, "settings");
 		outStream.close();
@@ -60,6 +69,13 @@ public class settingsService<E> {
 	
 	private String getFilename() {
 		return System.getProperty("user.home")+File.separator+".webapp"+File.separator+"webapp.preferences";
+	}
+	
+	private String caps(String string) {
+		char[] charArray = string.toCharArray();
+		charArray[0] = Character.toUpperCase(charArray[0]);
+		String Key = new String(charArray);
+		return Key;
 	}
 	
 }
