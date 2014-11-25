5b5c174952435457af325c8df3d1325059e14d60
Kleber Mota <klebermo@gmail.com>
2014-11-25 07:51:30 -0300

    updating security context

--- src/main/java/com/spring/config/security/SecurityConfig.java
+++ src/main/java/com/spring/config/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.PermissionEvaluator;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -34,6 +35,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
+		DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
+		expressionHandler.setPermissionEvaluator(permissionEvaluator);
+		
     	http
     		.csrf()
     			.disable()
@@ -60,7 +64,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(WebSecurity web) throws Exception {
 		DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
 	    handler.setPermissionEvaluator(permissionEvaluator);
-	    web.expressionHandler(handler);
+	    
+	    web
+	    	.expressionHandler(handler);
     }
 	
 	@Override
